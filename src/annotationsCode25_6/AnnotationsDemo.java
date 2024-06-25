f81a2b05ea8c94324e79a032881446a892bd72ba
yumnamansuri <169126986+yumnamansuri@users.noreply.github.com>
2024-06-25 11:32:20 +0530

    25-6-24

---
+++ src/annotationsCode25_6/AnnotationsDemo.java
@@ -0,0 +1,24 @@
+package annotationsCode25_6;
+
+
+
+
+public class AnnotationsDemo {
+
+	public static void main(String[] args) {
+		
+		/*
+
+		Symbol of Annotations is @
+		
+		5 types of annotations
+		-> Marker  annotations
+		-> Full annotations
+		-> Single annotations
+		-> Type annotations
+		-> Repeating annotations
+
+		*/
+	}
+
+}
---
+++ src/annotationsCode25_6/Junit.java
@@ -0,0 +1,19 @@
+package annotationsCode25_6;
+
+public class Junit {
+
+	public static void main(String[] args) {
+		
+		/*
+		 Free, Open source, software testing, framework for java
+		 Junit 5 have new features compare to junit 3 & 4 
+		 
+		  
+		 --> Unit Testing
+		 
+		 
+		 	
+		 */
+	}
+
+}
